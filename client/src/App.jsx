d93fe45154adf0818412e55fbec4b348be52771e
Divyanshu Yadav <divyanshu.1810@gmail.com>
2024-08-08 14:43:20 +0530

    feat: add login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,59 +1,65 @@
-import { createBrowserRouter,RouterProvider } from 'react-router-dom'
-import {LayOut,LoginRequiredLayout} from './components/outLet/OutLet'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { LayOut, LoginRequiredLayout } from './components/outLet/OutLet'
 import ManagerDashboard from './pages/Dashboards/manager/ManagerDashboard'
 import CrewDashboard from './pages/Dashboards/crew/CrewDashboard'
 import DriverDashboard from './pages/Dashboards/driver/DriverDashboard'
 import SupervisorDashboard from './pages/Dashboards/supervisor/SupervisorDashboard'
 import Landing from './pages/Landing/Landing'
 import './App.css'
+import { Login } from './pages/Login/Login'
 
 function App() {
   const router = createBrowserRouter([
     {
-      path:'/',
-      element:<LayOut/>,
-      children:[
+      path: '/',
+      // element:<LayOut/>,
+      children: [
         {
-          path:'/',
-          element:<Landing/>
+          path: '/',
+          element: <Landing />
+          // element: <h1>hello</h1>
+        },
+        {
+          path: '/login',
+          element: <Login />
         }
       ]
     },
     {
-      path:'/',
-      element:<LoginRequiredLayout/>,
-      children:[
+      path: '/',
+      element: <LoginRequiredLayout />,
+      children: [
         {
-          path:'/manager',
-          element:<ManagerDashboard/>,
-          children:[
+          path: '/manager',
+          element: <ManagerDashboard />,
+          children: [
             {
-              path:":page",
-              element:<ManagerDashboard/>
+              path: ":page",
+              element: <ManagerDashboard />
             }
           ]
         },
         {
-          path:'/crew',
-          element:<CrewDashboard/>,
-          children:[
+          path: '/crew',
+          element: <CrewDashboard />,
+          children: [
             {
-              path:":page",
-              element:<CrewDashboard/>
+              path: ":page",
+              element: <CrewDashboard />
             }
           ]
         },
         {
-          path:'/driver',
-          element:<DriverDashboard/>
+          path: '/driver',
+          element: <DriverDashboard />
         },
         {
-          path:'/supervisor/',
-          element:<SupervisorDashboard/>,
-          children:[
+          path: '/supervisor/',
+          element: <SupervisorDashboard />,
+          children: [
             {
-              path:":page",
-              element:<SupervisorDashboard/>
+              path: ":page",
+              element: <SupervisorDashboard />
             }
           ]
         },
@@ -63,7 +69,7 @@ function App() {
   ])
 
   return (
-    <RouterProvider router={router}/>
+    <RouterProvider router={router} />
   )
 }
 
